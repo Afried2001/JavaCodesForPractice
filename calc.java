480782b1a1753145888231fb7213af1608e42dbf
root <root@ip-172-31-46-84.ap-south-1.compute.internal>
2026-05-21 16:42:07 +0000

    added mutliplication functionality

--- calc.java
+++ calc.java
@@ -1,8 +1,9 @@
 public class calc{
 	public static void main(String[] args){
 		int a=10;
-		int b=21;
+		int b=9;
 		System.out.println("The Sum is:"+(a+b));
+		System.out.println("Product is:"+(a*b));
 	}
 }
 
