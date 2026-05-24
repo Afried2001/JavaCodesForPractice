5e3855e63ff498d4ea3f48e3e565db299e5a7a79
root <root@ip-172-31-46-84.ap-south-1.compute.internal>
2026-05-24 16:20:22 +0000

    modified calc.java

--- calc.java
+++ calc.java
@@ -1,9 +1,11 @@
 public class calc{
 	public static void main(String[] args){
-		int a=10;
-		int b=9;
+		int a=9;
+		int b=6;
+		System.out.println("a is "+a+", b is "+b);
 		System.out.println("The Sum is:"+(a+b));
 		System.out.println("Product is:"+(a*b));
+		System.out.println("The difference is:"+(a-b));
 	}
 }
 
