46221c98514c3dba9f33325607c4eaec5b6214ad
root <root@ip-172-31-46-84.ap-south-1.compute.internal>
2026-05-24 16:29:54 +0000

    modified a and b values in calc.java file

--- calc.java
+++ calc.java
@@ -1,7 +1,7 @@
 public class calc{
 	public static void main(String[] args){
-		int a=9;
-		int b=6;
+		int a=15;
+		int b=9;
 		System.out.println("a is "+a+", b is "+b);
 		System.out.println("The Sum is:"+(a+b));
 		System.out.println("Product is:"+(a*b));
