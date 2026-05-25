5e447c64e05af7e626a309609618eeb34eca3859
Afried2001 <160830050+Afried2001@users.noreply.github.com>
2026-05-25 19:05:13 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,7 +1,7 @@
 public class calc{
 	public static void main(String[] args){
-		int a=9;
-		int b=2;
+		int a=20;
+		int b=10;
 		System.out.println("a is "+a+", b is "+b);
 		System.out.println("The Sum is:"+(a+b));
 		System.out.println("Product is:"+(a*b));
